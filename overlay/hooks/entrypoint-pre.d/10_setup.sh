fb16ee3ad672a0290939a3316a82a4cc31c577a4
bkleiner <b.kleiner@str8labs.com>
2019-02-02 15:49:36 +0100

    fix setup path

--- overlay/hooks/entrypoint-pre.d/10_setup.sh
+++ overlay/hooks/entrypoint-pre.d/10_setup.sh
@@ -7,5 +7,5 @@ sed -i "s/CACHE_MEM_SIZE/${CACHE_MEM_SIZE}/"  /etc/nginx/conf.d/20_proxy_cache_p
 sed -i "s/CACHE_DISK_SIZE/${CACHE_DISK_SIZE}/" /etc/nginx/conf.d/20_proxy_cache_path.conf
 sed -i "s/CACHE_MAX_AGE/${CACHE_MAX_AGE}/"    /etc/nginx/sites-available/generic.conf.d/root/20_cache.conf
 sed -i "s/UPSTREAM_DNS/${UPSTREAM_DNS}/"    /etc/nginx/sites-available/generic.conf.d/10_generic.conf
-sed -i "s/LISTEN_ADDR/${LISTEN_ADDR}/"    /etc/nginx/sites-available/generic.conf.d/10_generic.conf
+sed -i "s/LISTEN_ADDR/${LISTEN_ADDR}/"    /etc/nginx/sites-available/10_generic.conf
 
